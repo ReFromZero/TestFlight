e3af8e7eb7063953d65580d32d0cf7ad27f78d27
ReFromZero <156634312+ReFromZero@users.noreply.github.com>
2025-01-22 01:26:20 +0900

    Add files via upload

--- tracks.js
+++ tracks.js
@@ -3,37 +3,37 @@
 const tracks = [
   {
     artwork:
-      "https://cdn.pixabay.com/audio/2022/05/27/23-46-21-714_200x200.jpg",
+      "./album/1.jpg",
     name: "anpanman",
     artist: "Zakeboys",
     path: "./media/anpanman.mp3",
   },
   {
     artwork:
-      "https://cdn.pixabay.com/audio/2023/10/24/15-08-22-671_200x200.jpg",
+      "./album/2.jpg",
     name: "yurilove",
     artist: "Zakeboys",
     path: "./media/yurilove.mp3",
   },
   {
     artwork:
-      "https://cdn.pixabay.com/audio/2023/07/22/02-53-18-138_200x200.jpg",
+      "./album/3.jpg",
     name: "weddingsong",
     artist: "Zakeboys",
     path: "./media/weddingsong.mp3",
   },
   {
     artwork:
-      "https://cdn.pixabay.com/audio/2023/03/19/12-27-22-207_200x200.jpg",
-    name: "Smoke",
-    artist: "Soul Prod Music",
-    path: "./media/143172.mp3",
+      "./album/4.jpg",
+    name: "yuriloveshort",
+    artist: "Zakeboys",
+    path: "./media/yuriloveshort.mp3",
   },
   {
     artwork:
-      "https://cdn.pixabay.com/audio/2022/04/13/11-20-13-185_200x200.jpg",
-    name: "Electronic Rock",
-    artist: "Alex Grohl",
-    path: "./media/15045.mp3",
+      "./album/5.jpg",
+    name: "anpanman2",
+    artist: "Zakeboys",
+    path: "./media/yuriloveshort.mp3",
   },
 ];
